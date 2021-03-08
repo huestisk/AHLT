b3cd63db285339f0eb54a0c47a033430eb324f17
Kevin Huestis <huestiskevin19@gmail.com>
2021-03-08 13:38:49 +0100

    Eval Error

--- classifier.py
+++ classifier.py
@@ -5,14 +5,14 @@ import numpy as np
 from sklearn.svm import SVC, LinearSVC
 from sklearn.preprocessing import OneHotEncoder
 
-outfile = 'results.out' # TODO: read from commandline
+outfile = 'results.out'  # TODO: read from commandline
 
 # delete old file
-if os.path.exists(outfile): 
+if os.path.exists(outfile):
     os.remove(outfile)
 
 # Load data
-datafile = 'devel.feat' # TODO: read from commandline
+datafile = 'devel.feat'  # TODO: read from commandline
 with open(datafile, 'r') as f:
     data = f.readlines()
 
@@ -34,8 +34,8 @@ print("Test Accurracy: {}".format(test_acc))
 # Print to file
 results = svm.predict(X)
 for idx, token in enumerate(data):
-    line = "{}|{}-{}|{}|{}".format(
-        token[0], token[2], token[3], token[1], results[idx]
-    )
+    line = "{}|{}-{}|{}|{}".format(token[0],
+                                   token[2], token[3], token[1], results[idx])
+    # FIXME: Remove backslashes, Duplicates
     with open(outfile, 'a') as f:
         print(line, file=f)
