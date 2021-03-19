4cca7790e3510400aabfc11db1d2d2822b5b05fe
Kevin Huestis <huestiskevin19@gmail.com>
2021-03-19 19:35:19 +0100

    Fix Eval Error

--- classifier.py
+++ classifier.py
@@ -1,18 +1,20 @@
 import os
+import sys
 import pickle
 import numpy as np
 
 from sklearn.svm import SVC, LinearSVC
 from sklearn.preprocessing import OneHotEncoder
 
-outfile = 'results.out'  # TODO: read from commandline
 
+# Output file
+outfile = sys.argv[2]
 # delete old file
 if os.path.exists(outfile):
     os.remove(outfile)
 
 # Load data
-datafile = 'devel.feat'  # TODO: read from commandline
+datafile = sys.argv[1]
 with open(datafile, 'r') as f:
     data = f.readlines()
 
@@ -33,9 +35,35 @@ print("Test Accurracy: {}".format(test_acc))
 
 # Print to file
 results = svm.predict(X)
-for idx, token in enumerate(data):
-    line = "{}|{}-{}|{}|{}".format(token[0],
-                                   token[2], token[3], token[1], results[idx])
-    # FIXME: Remove backslashes, Duplicates
+
+idx = results != '0'
+tokens = data[idx, :4]
+labels = results[idx]
+
+lines = set()
+docId = None
+
+for token, label in zip(tokens, labels):
+    
+    if token[0] != docId:        # if new doc, then can't be the same name
+        docId = token[0]
+
+    pos, fLabel = label.split('-')
+
+    start = token[2]
+    end = token[3]
+    name = token[1]
+
+    lines.add(
+        "{}|{}-{}|{}|{}".format(docId, start, end, name, fLabel)
+    )
+# else:
+#     lines.append(
+#         "{}|{}-{}|{}|{}".format(docId, start, end, name, final_label)
+#     )
+
+
+# FIXME: combine results
+for line in lines:
     with open(outfile, 'a') as f:
         print(line, file=f)
