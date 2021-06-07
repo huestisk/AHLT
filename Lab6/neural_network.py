0280081ef6e700ea709e53e2d3e18935400c8652
betty <et16btt@cs.umu.se>
2021-06-07 14:40:55 +0200

    "not sure what change I made here.."

--- Lab6/neural_network.py
+++ Lab6/neural_network.py
@@ -172,3 +172,21 @@ def load_model_and_indexs(filename):
 
     return model, idx
 
+import numpy as np
+
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1
+    # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath) as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word]
+                embedding_matrix[idx] = np.array(
+                                        vector, dtype=np.float32)
+                                        [:embedding_dim]
+
+    return embedding_matrix
+
