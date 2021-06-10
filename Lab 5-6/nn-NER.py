351d017460c03ad1d70918bede0c2bd4c70a0a48
huestisk <huestiskevin19@gmail.com>
2021-06-10 14:49:28 +0200

    Automatic Logging + Eval

--- Lab 5-6/nn-NER.py
+++ Lab 5-6/nn-NER.py
@@ -1,8 +1,11 @@
-#from eval.evaluator import evaluate
-
 import sys
 import numpy as np
+import pandas as pd
 from datetime import datetime
+from contextlib import redirect_stdout
+
+sys.path.append(sys.path[0] + '/../eval/')
+from evaluate import evaluate
 
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
@@ -29,7 +32,7 @@ testDir = sys.argv[2]
 # timestamp
 dateTimeObj = datetime.now()
 timestampStr = dateTimeObj.strftime("%Y-%b-%d-%H:%M")
-# TODO: log training
+logfile = 'logs/NER_' + timestampStr + '.log'
 
 """ Learn """
 if learn:
@@ -49,9 +52,17 @@ if learn:
     # build network
     model = build_network(idx)
 
+    with open(logfile, 'w') as f:
+        with redirect_stdout(f):
+            model.summary()
+        print('\n\n',file=f)
+
     # train model
-    model.fit(X_train, y_train, epochs=5, validation_data=(
+    history = model.fit(X_train, y_train, epochs=5, validation_data=(
         X_test, y_test), validation_steps=1, verbose=2)
+
+    hist_df = pd.DataFrame(history.history)
+    hist_df.to_csv(logfile, mode='a', encoding='utf-8')
 
     # save model and indices, for later use in prediction
     save_model_and_indices(model, idx, MODEL_NAME)
@@ -78,5 +89,5 @@ if predict:
     output_entities(valData, y_pred, 'logs/' + outfile)
 
     # evaluate using official evaluator
-    # evaluation(datadir, outfile)
+    evaluate('NER', testDir, outfile)
 
--- eval/evaluate.py
+++ eval/evaluate.py
@@ -1,23 +1,35 @@
-import sys
+import platform
 import subprocess
+from contextlib import redirect_stdout
 
-# parse arguments
-datadir = sys.argv[1]
-outfile = sys.argv[2]
+def evaluate(type: str, datadir: str, outfile: str) -> None:
+    # parse files
+    logfile = 'logs/' + outfile[:-4] + '.log'
 
-# on linux
-from evaluator import evaluate
-evaluate('DDI', datadir, outfile)
+    # on linux
+    if platform.system() == 'Linux':
+        from eval.evaluator import evaluate
+        evaluate(type, datadir, outfile)
 
-# Send file to Raspberry Pi
-cmd = "scp /Users/kevinhuestis/Development/AHLT/{} pi@192.168.1.123:/home/pi/AHLT/{}".format(
-    outfile, outfile)
-subprocess.Popen([cmd], shell=True)
+    # on kevin's mac
+    elif platform.system() == 'Darwin':
+    
+        # Send file to Raspberry Pi
+        cmd = "scp /Users/kevinhuestis/Development/AHLT/logs/{} pi@192.168.1.123:/home/pi/AHLT/".format(
+            outfile, outfile)
+        subprocess.Popen([cmd], shell=True)
 
-""" Connect to Raspberry pi for eval """
-# Note: Pub key must be added to raspberry pi
-user = 'pi'
-host = '192.168.1.123'
-cmd = "python3 AHLT/evaluator.pyc DDI AHLT/{} AHLT/{}".format(datadir, outfile)
-ssh_cmd = 'ssh %s@%s "%s"' % (user, host, cmd)
-p = subprocess.Popen([ssh_cmd], shell=True)
+        """ Connect to Raspberry pi for eval """
+        # Note: Pub key must be added to raspberry pi
+        user = 'pi'
+        host = '192.168.1.123'
+        cmd = "python3 AHLT/evaluator.pyc {} AHLT/{} AHLT/{}".format(type, datadir, outfile)
+        ssh_cmd = 'ssh %s@%s "%s"' % (user, host, cmd)
+
+        with open(logfile, 'a') as f:
+            print('\n\nResults:', file=f)
+            p = subprocess.Popen([ssh_cmd], shell=True, stdout=f, stderr=f)
+
+if __name__ == "__main__":
+    evaluate('DDI', 'data/devel', 'DDI_2021-Jun-10-14:39.out')
+    evaluate('NER', 'data/devel', 'NER_2021-Jun-10-14:33.out')
